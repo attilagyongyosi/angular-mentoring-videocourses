56fdf5a334b57cb8e1c652ec06533cb2a81cafed
attilagyongyosi <attilagyongyosi0@gmail.com>
2018-06-27 22:04:58 +0200

    Switching over to the SASS preprocessor.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'vc-app',
     templateUrl: './app.component.html',
-    styleUrls: [ './app.component.css' ]
+    styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent {
     public title = 'angular-mentoring-videocourses';
