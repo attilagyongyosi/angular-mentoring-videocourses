8e86a528877a2c6ff7e3b2c77a6d55d3ed69a1d6
attilagyongyosi <attilagyongyosi0@gmail.com>
2018-07-27 19:50:34 +0200

    Setting up proper routing, Add Course button handling.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,10 @@ import { CreateCoursePageComponent } from './modules/create-course/pages/create-
 import { NotFoundPageComponent } from './modules/not-found/pages/not-found.page';
 
 export const ROUTES: Route[] = [
-    { path: '', redirectTo: '/courses', pathMatch: 'full' },
     { path: 'login', component: LoginPageComponent },
     { path: 'courses', component: CoursesPageComponent },
-    { path: 'courses/:id', component: CoursesPageComponent },
     { path: 'courses/new', component: CreateCoursePageComponent },
+    { path: 'courses/:id', component: NotFoundPageComponent },
+    { path: '', redirectTo: '/courses', pathMatch: 'full' },
     { path: '**', component: NotFoundPageComponent }
 ];
--- src/app/modules/courses/components/action-bar/action-bar.component.ts
+++ src/app/modules/courses/components/action-bar/action-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SearchService } from '../../../shared/services/search.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'vc-action-bar',
@@ -7,10 +8,15 @@ import { SearchService } from '../../../shared/services/search.service';
     styleUrls: [ './action-bar.component.scss' ]
 })
 export class ActionBarComponent {
-    constructor(private searchService: SearchService) {}
+    constructor(private router: Router,
+                private searchService: SearchService) {}
 
     public emitSearch(term: string) {
         this.searchService.search(term);
+    }
+
+    public newCourse(): void {
+        this.router.navigateByUrl('/courses/new');
     }
 
 }
