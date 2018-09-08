65ce4b73b828c38ca2e4f2bf8b42df4860565bc4
attilagyongyosi <attilagyongyosi0@gmail.com>
2018-09-08 14:28:20 +0200

    AuthGuard with Observable return value.

--- src/app/modules/shared/auth/auth.guard.ts
+++ src/app/modules/shared/auth/auth.guard.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivateChild, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from './services/auth.service';
+import { Observable, of } from 'rxjs';
 
 @Injectable()
 export class AuthGuard implements CanActivateChild {
     constructor(private authService: AuthService,
                 private router: Router) {}
 
-    public canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    public canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
         if (!this.authService.isAuthenticated()) {
             this.router.navigate([ '/login' ]);
-            return false;
+            return of(false);
         }
 
-        return true;
+        return of(true);
     }
 
 }
